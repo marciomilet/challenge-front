38d0591104f182bf02dcb6478074550cc71797da
marcio.milet <marcio.milet@sad.pe.gov.br>
2025-10-12 13:11:15 -0300

    feat simple header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,11 +1,15 @@
+import { NavLink } from "./nav-link";
 
 
 
 export function Header() {
     return (
-        <div>
-            <nav>
-                
+        <div className="flex items-center gap-5 py-2">
+            <nav className="flex items-center gap-5">
+                <NavLink href="/">Home</NavLink>
+                <NavLink href="/upload">Upload</NavLink>
+                <NavLink href="/users">Users</NavLink>
+                <NavLink href="/campaigns">Campaigns</NavLink>
             </nav>
         </div>
     )
---
+++ src/components/nav-link.tsx
@@ -0,0 +1,11 @@
+import type {ComponentProps} from "react" 
+
+interface NavLinkProps extends ComponentProps<'a'> {
+    children: string
+}
+
+export function NavLink(props: NavLinkProps) {
+    return (
+        <a {...props } className="font-medium text-sm"> {props.children} </a>
+    )
+}
