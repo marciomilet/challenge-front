9ac351e1c421ea404993a138067a558aae90c14d
marcio.milet <marcio.milet@sad.pe.gov.br>
2025-10-13 17:00:23 -0300

    Update App Components

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,11 @@
 import { Header } from "./components/header"
-import { Table } from "./components/table/table"
-import { TableCell } from "./components/table/table-cell"
-import { TableHeader } from "./components/table/table-header"
-import { TableTitleSearch } from "./components/table/table-title-search"
+import { UsersList } from "./components/Users-list"
 
 export function App() {
   return (
-    <div>
+    <div className="max-w-[1216] mx-auto py-5 flex flex-col gap-5">
       <Header />
-      <TableTitleSearch children="Users" />
-      <Table>
-        <thead>
-          <tr>
-            <TableHeader>ID</TableHeader>
-            <TableHeader>Name</TableHeader>
-            <TableHeader>Age</TableHeader>
-            <TableHeader>Income</TableHeader>
-            <TableHeader>City</TableHeader>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <TableCell>1</TableCell>
-            <TableCell>John Doe</TableCell>
-            <TableCell>30</TableCell>
-            <TableCell>$50,000</TableCell>
-            <TableCell>New York</TableCell>
-          </tr>
-          <tr>
-            <TableCell>2</TableCell>
-            <TableCell>Jane Smith</TableCell>
-            <TableCell>25</TableCell>
-            <TableCell>$60,000</TableCell>
-            <TableCell>Los Angeles</TableCell>
-          </tr>
-        </tbody>
-        
-        <tfoot>
-          <tr>
-            <TableCell colSpan={5} className="text-center text-sm text-zinc-400 py-2">
-              End of Table
-            </TableCell>
-          </tr>
-        </tfoot>
-
-      </Table>
-      
+      <UsersList />
     </div>
   )
 }
