25fdf1f88ac7ab1a987a3c872a93aa40b1e5a02d
marcio.milet <marcio.milet@sad.pe.gov.br>
2025-10-15 11:31:52 -0300

    update header style

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,11 +2,11 @@ import { Link } from "react-router-dom";
 
 export function Header() {
     return (
-        <div className="flex items-center gap-5 py-2 px-5">
+        <div className="flex items-center justify-between gap-5 py-2 px-5 bg-zinc-900 border-b border-zinc-800">
             <nav className="flex items-center gap-5">
-                <Link to="/upload">Upload</Link>
-                <Link to="/users">Users</Link>
-                <Link to="/campaigns">Campaigns</Link>
+                <Link to="/upload" className="hover:text-zinc-300 transition-colors">Upload</Link>
+                <Link to="/users" className="hover:text-zinc-300 transition-colors">Users</Link>
+                <Link to="/campaigns" className="hover:text-zinc-300 transition-colors">Campaigns</Link>
             </nav>
         </div>
     )
